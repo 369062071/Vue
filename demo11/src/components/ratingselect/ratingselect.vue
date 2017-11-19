d1fa07a58077183e5f1ed551d1f1f5bbcc97a360
LiangBo <369062071@qq.com>
2017-11-19 23:45:43 +0800

    1
    1

--- demo11/src/components/ratingselect/ratingselect.vue
+++ demo11/src/components/ratingselect/ratingselect.vue
@@ -16,7 +16,7 @@
             
         
         </div>
-        <div class="switch">
+        <div class="switch" :class="{ 'on' : onlyContent }">
             <span class="icon-check_circle"></span>
             <span class="text">只看有内容的评价</span>
         </div>
@@ -62,7 +62,7 @@
             margin 0 18px
             border-1px(raba(7,17,27,.1))
             font-size 0
-            .block
+            .block 
                 display inline-block
                 padding 8px 12px
                 margin-right 8px
@@ -82,5 +82,23 @@
                     background rgba(77,85,93,.2)
                     &.active
                         background rgb(77,85,93)
+        .switch
+            padding 12px 18px
+            line-height 24px
+            border-bottom 1px solid rgba(7,17,27,.1)
+            color rgb(147,153,159)
+            font-size 0
+            &.on 
+                .icon-check_circle
+                    color #00c850
+            .icon-check_circle 
+                display inline-block
+                vertical-align top
+                margin-right 4px
+                font-size 24px
+            .text
+                display inline
+                vertical-align top
+                font-size 12px
 
 </style>
