49b0b73cd81019787691fc3ccdc881eab27c29dd
LiangBo <369062071@qq.com>
2017-12-13 00:02:19 +0800

    bug

--- vue-music/src/App.vue
+++ vue-music/src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
     <m-header></m-header>
-    <router-view/>
+    <tab></tab>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 import MHeader from './components/m-header/m-header'
+import Tab from './components/tab/tab'
+
 export default {
   components: {
-    MHeader
+    MHeader,
+    Tab
   }
 }
 </script>
 
-<style scoped lang="stylus">
-  
+<style  lang="stylus">
+  @import './common/stylus/reset';
 </style>
+
--- vue-music/src/router/index.js
+++ vue-music/src/router/index.js
@@ -24,6 +24,10 @@ export default new Router({
     {
       path: '/search',
       component: Search
+    },
+    {
+      path: '/',
+      redirect: '/recommend'
     }
   ]
 })
