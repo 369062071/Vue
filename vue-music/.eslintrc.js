1ff759b2c345bdac62d0db8b380f6e29c2cb8410
LiangBo <369062071@qq.com>
2017-12-13 18:40:18 +0800

    1

--- vue-music/.eslintrc.js
+++ vue-music/.eslintrc.js
@@ -20,6 +20,8 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "camelcase": 2,//强制驼峰法命名
+    // "semi": [2, "always"],//语句强制分号结尾
   }
 }
